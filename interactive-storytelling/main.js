c2c902f64b00b7e54bb10c8241033c1c6038408b
nandithapaila-24 <nandithapaila@gmail.com>
2025-11-25 09:52:35 -0500

    interactive storytelling project

---
+++ interactive-storytelling/main.js
@@ -0,0 +1,10 @@
+let createButton = document.getElementById("create-button");
+let firstSelectionButton = document.getElementById("first-letter-button");
+let secondSelectionButton = document.getElementById("second-letter-button");
+let backButton = document.getElementById("back-button");
+
+let create = document.getElementById("create");
+let first = document.getElementById("first-selection");
+let second = document.getElementById("second-selection");
+let design = document.getElementById("decorated-sign");
+
