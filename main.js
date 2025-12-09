d72866e69c04287b5c05833d1919b5c980b1ea32
nandithapaila-24 <nandithapaila@gmail.com>
2025-12-09 08:14:43 -0500

    Add files via upload
    web to print code

--- main.js
+++ main.js
@@ -1,10 +1,71 @@
-let keyboard = document.getElementById("keyboard-graphic");
-let body = document.body;
-let h1 = document.querySelector("h1");
+let button = document.getElementById("pink-mode");
+let button2 = document.getElementById("green-mode");
+let button3 = document.getElementById("font-mode");
+let page = document.body;
 
-keyboard.addEventListener("click", function(event) {
-    
-    /* change background color and font color of body */
-    body.classList.toggle("dark-mode");
-    h1.classList.toggle("dark-mode");
-});
+button.addEventListener("click", function(){
+    page.classList.toggle("switch-mode")
+})
+button2.addEventListener("click", function(){
+    page.classList.toggle("green-switch-mode")
+})
+
+// button3.addEventListener("change", function(){
+//     let selectedFont = title.value;
+//     page.style.fontFamily = selectedFont;
+// })
+
+let dropdown = document.getElementById("desktop");
+let img = document.getElementById("mobile");
+
+
+dropdown.addEventListener("change", function(){
+    /*first get what the user selected*/
+
+    let val = dropdown.value
+
+    /*then  change the image*/
+
+    img.src = val;
+})
+
+let nametextInput = document.getElementById("name-text");
+let taglinetextInput = document.getElementById("tagline-text");
+let title = document.getElementById("title");
+let tagline = document.getElementById("tagline");
+let fonttextInput = document.getElementById("font-text");
+
+
+
+nametextInput.addEventListener("input", function(){
+    let userText = nametextInput.value;
+    title.innerHTML = userText;
+})
+
+taglinetextInput.addEventListener("input", function(){
+    let userText = taglinetextInput.value;
+    tagline.innerHTML = userText;
+})
+
+let colorInput = document.getElementById("color");
+
+colorInput.addEventListener("input", function(){
+    let userColor = colorInput.value;
+    page.style.backgroundColor = userColor;
+    /* this is saying go into the style of the page and make the background color equal the user color*/
+})
+
+
+
+let downloadButton = document.getElementById("download");
+
+downloadButton.addEventListener("click", function(){
+    window.print();
+    page.style.backgroundColor = userColor;
+})
+
+if (selectedFont) {
+    posterText.style.fontFamily = '"${selectedFont}", system-ui,sans-serif';
+} else {
+    posterText.style.fontFamily = "font-name";
+}
